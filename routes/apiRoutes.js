cdc6f259a46fe9a55c46a09abbf21d7ddbea8101
mapachurro <oliver.renwick@gmail.com>
2019-02-16 12:16:53 -0500

    Created random search query in GET route, adjusted GET/POST routes to new model name

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // This selects a single, random, entry to be loaded as an article to the page
+  app.get("/api/newPage", function(req, res) {
+   db.Articles.findOne({ order: 'rand()' }).then(function(dbArticles) {
+      res.json(dbArticles);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // This route allows a user to save their new article or version of an article to the database
+  app.post("/api/userSave", function(req, res) {
+    db.Example.create(req.body).then(function(dbArticles) {
+      res.json(dbArticles);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Articles.destroy({ where: { id: req.params.id } }).then(function(dbArticles) {
+      res.json(dbArticles);
     });
   });
 };
