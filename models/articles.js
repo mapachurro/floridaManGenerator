3499610821bd366381a224c7235a397d9ee0b678
Julia Magyar <magyar.juliar@gmail.com>
2019-02-16 13:10:45 -0500

    added articles model

--- models/articles.js
+++ models/articles.js
@@ -1,6 +1,17 @@
-module.exports = function(sequelize, DataTypes){
-    var Article = sequelize.define("Article",{
-        headline: DataTypes.STRING,
-        photoPath
-    })
-}
+module.exports = function(sequelize, DataTypes) {
+  var Article = sequelize.define("Article", {
+    headline: {
+      type: sequelize.STRING,
+      allowNull: false
+    },
+    photoPath: DataTypes.STRING,
+    articleText: {
+      type: sequelize.TEXT,
+      validate: {
+        len: [1, 5000]
+      }
+    },
+    originalURL: DataTypes.STRING
+  });
+  return Article;
+};
