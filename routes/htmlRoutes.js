7de22a1493137c5118bab86171a8d428dd4d6a71
mapachurro <oliver.renwick@gmail.com>
2019-02-23 13:58:01 -0500

    removing some unneccessary stuff

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,16 +5,6 @@ module.exports = function(app) {
     res.render("userCreate");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/articles/:id", function(req, res) {
-    db.Articles.findOne({ where: { id: req.params.id } }).then(function(
-      dbArticles
-    ) {
-      console.log(dbArticles);
-      res.render("articles", { articles: dbArticles });
-    });
-  });
-
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
