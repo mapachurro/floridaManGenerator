3af4b9003d1d79e429cfb4cdaf92c20e75999bdd
mapachurro <oliver.renwick@gmail.com>
2019-02-23 13:23:22 -0500

    small changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,6 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Articles.findAll({}).then(function(dbArticles) {
-      res.render("index", {
-        msg: "Welcome!",
-        articles: dbArticles
-      });
-    });
-  });
-
   // Load example page and pass in an example by id
   app.get("/articles/:id", function(req, res) {
     db.Articles.findOne({ where: { id: req.params.id } }).then(function(
