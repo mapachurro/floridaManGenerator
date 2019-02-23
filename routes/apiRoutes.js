0234b4b2e682ad132b42b5a11460f622fe59cff9
Julia Magyar <magyar.juliar@gmail.com>
2019-02-23 11:01:02 -0500

    prettier fixes for travis

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,6 @@ var Sequelize = require("sequelize");
 module.exports = function(app) {
   // This selects a single, random, entry to be loaded as an article to the page
   app.get("/api/newPage", function(req, res) {
-
     // eslint-disable-next-line prettier/prettier
     db.Articles.findOne({ order: [Sequelize.literal("RAND()")] }).then(function(
       dbArticles
