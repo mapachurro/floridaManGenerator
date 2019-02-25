e1065e9fbf916208de908e8724641a944bac97a0
Julia Magyar <magyar.juliar@gmail.com>
2019-02-25 18:18:28 -0500

    added header to stupidity page

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-    password: "Man0Negra",
+    password: process.env.MYSQL_PASSWORD,
     database: "floridaManDB",
     host: "127.0.0.1",
     dialect: "mysql"
